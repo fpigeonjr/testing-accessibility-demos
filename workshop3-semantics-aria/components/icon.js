d27331b68d9694e0c1ca7be506f6adb8c39f6cba
Frank Pigeon <frank.pigeonjr@gmail.com>
2022-02-16 15:49:12 -0600

    wip

--- workshop3-semantics-aria/components/icon.js
+++ workshop3-semantics-aria/components/icon.js
@@ -1,39 +1,44 @@
-import React, {useState} from "react"
-import './styles/icons.scss'
+import React, { useState } from "react"
+import "./styles/icons.scss"
 
-const Icon = ({name, showText = false}) => {
-    const StaticOrInteractive = showText ? `span` : `button`
-    const tooltip = showText ? false : true
-    const [tooltipActive, setTooltipActive] = useState(false)
-    const showTooltip = () => {
-        setTooltipActive(true)
-    }
-    const hideTooltip = () => {
-        setTooltipActive(false)
-    }
-    return (
-        <StaticOrInteractive
-            className={tooltip ? 'icon-btn' : 'wrap-icon-text'}
-            onMouseOver={tooltip ? showTooltip : null}
-            onMouseOut={tooltip ? hideTooltip : null}
-            onBlur={tooltip ? hideTooltip : null}
-            onFocus={tooltip ? showTooltip : null}
+const Icon = ({ name, showText = false }) => {
+  const StaticOrInteractive = showText ? `span` : `button`
+  const tooltip = showText ? false : true
+  const [tooltipActive, setTooltipActive] = useState(false)
+  const showTooltip = () => {
+    setTooltipActive(true)
+  }
+  const hideTooltip = () => {
+    setTooltipActive(false)
+  }
+  return (
+    <StaticOrInteractive
+      className={tooltip ? "icon-btn" : "wrap-icon-text"}
+      onMouseOver={tooltip ? showTooltip : null}
+      onMouseOut={tooltip ? hideTooltip : null}
+      onBlur={tooltip ? hideTooltip : null}
+      onFocus={tooltip ? showTooltip : null}
+    >
+      <span
+        aria-labelledby={`icon-${name}`}
+        role="img"
+        className={`icon-${name}`}
+      ></span>
+      {showText ? (
+        <span className="text" id={`icon-${name}`}>
+          {name}
+        </span>
+      ) : null}
+      {tooltip ? (
+        <span
+          id={`icon-${name}`}
+          className={`tooltip ${!showText && tooltipActive ? `active` : ``}`}
         >
-            <span
-                className={`icon-${name}`}
-            >
-            </span>
-            { showText ? <span className="text">{name}</span> : null }
-            { tooltip ?
-                <span
-                    className={
-                        `tooltip ${!showText && tooltipActive ? `active` : `` }`}
-                >
-                    {name}
-                </span>
-            : null }
-        </StaticOrInteractive>
-    )
+          {name}
+        </span>
+      ) : null}
+    </StaticOrInteractive>
+  )
 }
 
 export default Icon
--- workshop3-semantics-aria/components/page-listing-detail.js
+++ workshop3-semantics-aria/components/page-listing-detail.js
@@ -1,68 +1,77 @@
 import React from "react"
 import BodyClassName from "react-body-classname"
 import sanitizeHtml from "sanitize-html"
-import {Helmet} from "react-helmet"
+import { Helmet } from "react-helmet"
 import LoadedImageUrl from "./utils/loaded-image-url"
 
 import "./styles/page-listings.scss"
-
+import HeadingPortal from "workshop3-components/heading-portal"
 import Icon from "workshop3-components/icon"
 import ListingsData from "workshop3-data/listings.json"
 import DatePicker from "workshop3-components/date-picker/date-picker"
 
 import * as imageURLs from "../../images/listings/*.{png,jpg}"
 
-const Listing = props => {
-    const data = ListingsData.listings[props.id]
-    const headerImageUrl = LoadedImageUrl(imageURLs, data.detailHeaderImageSrc)
-    return (
-        <BodyClassName className="header-overlap page-listing-detail">
-            <>
-                <div>
-                    <div
-                        className="page-header"
-                        style={{backgroundImage: `url(${headerImageUrl}`}}
-                    >
-                        <div className="page-header-content wide-layout">
-                            <div className="listing-name">{data.listingName}</div>
-                            <p className="location">{data.location}</p>
-                        </div>
+const Listing = (props) => {
+  const data = ListingsData.listings[props.id]
+  const headerImageUrl = LoadedImageUrl(imageURLs, data.detailHeaderImageSrc)
+  return (
+    <BodyClassName className="header-overlap page-listing-detail">
+      <>
+        <HeadingPortal>
+          <h1 className="listing-name">Camp Spots{data.listingName}</h1>
+        </HeadingPortal>
+        <article>
+          <header
+            className="page-header"
+            style={{ backgroundImage: `url(${headerImageUrl}` }}
+          >
+            <div className="page-header-content wide-layout">
+              <p className="location">{data.location}</p>
+            </div>
+          </header>
+          <section className="wide-layout two-parts-70-30">
+            <div>
+              <h3>Description</h3>
+              <div
+                className="description-text"
+                dangerouslySetInnerHTML={{
+                  __html: sanitizeHtml(data.description)
+                }}
+              />
+              <h3>Amenities</h3>
+              <div className="amenity-icons grid">
+                {data.amenities.map((amenity, index) => {
+                  return (
+                    <div key={index}>
+                      <Icon name={amenity} showText={true} />
                     </div>
-                    <div className="wide-layout two-parts-70-30">
-                        <div>
-                            <div>Description</div>
-                            <div className="description-text" dangerouslySetInnerHTML={{__html: sanitizeHtml(data.description)}} />
-
-                            Amenities
-                            <div className="amenity-icons grid">
-                            {data.amenities.map((amenity, index) => {
-                                return <div key={index}>
-                                    <Icon name={amenity} showText={true} />
-                                </div>
-                            })}
-                            </div>
-                        </div>
-                        <div>
-                            Calendar
-                            <DatePicker />
-                        </div>
-                    </div>
-                    <div className="wide-layout">
-                        <div className="detail-images">
-                            {data.detailImages.map((image, index) => {
-                                let detailImageUrl = LoadedImageUrl(imageURLs, image.imageSrc)
-                                return <img
-                                    key={index}
-                                    src={detailImageUrl}
-                                    alt={image.imageAlt}
-                                />
-                            })}
-                        </div>
-                    </div>
-                </div>
-            </>
-        </BodyClassName>
-    )
+                  )
+                })}
+              </div>
+            </div>
+            <div>
+              <h3>
+                Calendar
+                <DatePicker />
+              </h3>
+            </div>
+          </section>
+          <div className="wide-layout">
+            <h3>Image Gallery</h3>
+            <div className="detail-images">
+              {data.detailImages.map((image, index) => {
+                let detailImageUrl = LoadedImageUrl(imageURLs, image.imageSrc)
+                return (
+                  <img key={index} src={detailImageUrl} alt={image.imageAlt} />
+                )
+              })}
+            </div>
+          </div>
+        </article>
+      </>
+    </BodyClassName>
+  )
 }
 
 export default Listing

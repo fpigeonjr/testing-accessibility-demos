f14b1c275bb10ea8d6af060bfc627980f5ef6c08
Frank Pigeon <frank.pigeonjr@gmail.com>
2022-02-23 16:02:44 -0600

    more a11y wins

--- workshop4-interactions-mechanics/components/slideshow/slideshow.js
+++ workshop4-interactions-mechanics/components/slideshow/slideshow.js
@@ -1,98 +1,140 @@
-import React, {useState, useRef} from "react"
+import React, { useState, useRef } from "react"
 import PropTypes from "prop-types"
 import LoadedImageUrl from "workshop4-components/utils/loaded-image-url"
 
 import "workshop4-components/slideshow/slideshow.scss"
 
-const Slideshow = ({images = [], imageURLs}) => {
-    let [currentSlideIndex, setCurrentSlideIndex] = useState(0)
-    let [fullScreenMode, setFullScreenMode] = useState(false)
-    
-    const btnFullScreenRef = useRef(null)
-    const btnCloseRef = useRef(null)
-    const slideshowRef = useRef(null)
+const Slideshow = ({ images = [], imageURLs }) => {
+  let [currentSlideIndex, setCurrentSlideIndex] = useState(0)
+  let [fullScreenMode, setFullScreenMode] = useState(false)
 
-    const decrementSlide = () => {
-        if (currentSlideIndex > 0) {
-            setCurrentSlideIndex(currentSlideIndex - 1)
-        } else {
-            setCurrentSlideIndex(images.length - 1)
-        }
-    }
-    const incrementSlide = () => {
-        if (currentSlideIndex < images.length - 1) {
-            setCurrentSlideIndex(currentSlideIndex + 1)
-        } else {
-            setCurrentSlideIndex(0)
-        }
-    }
-    const changeSlide = (index) => {
-        setCurrentSlideIndex(index)
-    }
-    const enterFullScreen = () => {
-        setFullScreenMode(true)
-    }
-    const closeFullScreen = () => {
-        setFullScreenMode(false)
-    }
-    const handleScreenClick = (event) => {
-        if (!slideshowRef.current.contains(event.target)) {
-            setFullScreenMode(false)
-        }
-    }
+  const btnFullScreenRef = useRef(null)
+  const btnCloseRef = useRef(null)
+  const slideshowRef = useRef(null)
 
-    return (
-        <>
-            <div
-                className="btn-slideshow-fullscreen" 
-                onClick={enterFullScreen}
-                ref={btnFullScreenRef}
-            >
-                <span className="icon"></span>
-            </div>
-            <div
-                className={`inspiration-slideshow ${fullScreenMode ? 'fullscreen' : ''}`}
-                onClick={(event)=>handleScreenClick(event)}
-            >
-                <div className="slideshow-container" ref={slideshowRef}>
-                    {images.map((image, index) => {
-                        const imageUrl = imageURLs ? LoadedImageUrl(imageURLs, image.src) : image.src
-                        return (
-                            <div className={`slide fade ${currentSlideIndex === index ? 'active' : ''}`} key={index}>
-                                <div className="numbertext">{index + 1} / {images.length}</div>
-                                <img src={imageUrl} alt={image.alt} style={{width: "100%"}} />
-                                <div className="text">{image.caption}</div>
-                            </div>
-                        )
-                    })}
+  const decrementSlide = () => {
+    if (currentSlideIndex > 0) {
+      setCurrentSlideIndex(currentSlideIndex - 1)
+    } else {
+      setCurrentSlideIndex(images.length - 1)
+    }
+  }
+  const incrementSlide = () => {
+    if (currentSlideIndex < images.length - 1) {
+      setCurrentSlideIndex(currentSlideIndex + 1)
+    } else {
+      setCurrentSlideIndex(0)
+    }
+  }
+  const changeSlide = (index) => {
+    setCurrentSlideIndex(index)
+  }
+  const enterFullScreen = () => {
+    setFullScreenMode(true)
+  }
+  const closeFullScreen = () => {
+    setFullScreenMode(false)
+  }
+  const handleScreenClick = (event) => {
+    if (!slideshowRef.current.contains(event.target)) {
+      setFullScreenMode(false)
+    }
+  }
 
-                    <a className="prev" onClick={()=>decrementSlide()}>&#10094;</a>
-                    <a className="next" onClick={()=>incrementSlide()}>&#10095;</a>
+  const slideshowKeyHandler = (event) => {
+    event.preventDefault()
+    if (fullScreenMode) {
+      if (event.key === "Escape") {
+        closeFullScreen()
+      }
+      if (event.key === "ArrowRight") {
+        incrementSlide()
+      }
+      if (event.key === "ArrowLeft") {
+        decrementSlide()
+      }
+    }
+  }
+
+  React.useEffect(() => {
+    if (fullScreenMode) {
+      slideshowRef.current.focus()
+    }
+  }, [currentSlideIndex])
+  return (
+    <>
+      <button
+        className="btn-slideshow-fullscreen"
+        onClick={enterFullScreen}
+        ref={btnFullScreenRef}
+      >
+        <span className="icon"></span>
+      </button>
+      <div
+        aria-live="polite"
+        className={`inspiration-slideshow ${
+          fullScreenMode ? "fullscreen" : ""
+        }`}
+        onClick={(event) => handleScreenClick(event)}
+        onKeyUp={slideshowKeyHandler}
+        ref={slideshowRef}
+        tabIndex="-1"
+        role={fullScreenMode ? "application" : "region"}
+        aria-roledescription="Image Slideshow"
+      >
+        <div className="slideshow-container" ref={slideshowRef}>
+          {images.map((image, index) => {
+            const imageUrl = imageURLs
+              ? LoadedImageUrl(imageURLs, image.src)
+              : image.src
+            return (
+              <figure
+                className={`slide fade ${
+                  currentSlideIndex === index ? "active" : ""
+                }`}
+                key={index}
+              >
+                <div className="numbertext">
+                  {index + 1} / {images.length}
                 </div>
-                <br />
+                <img src={imageUrl} alt={image.alt} style={{ width: "100%" }} />
+                <div className="text">{image.caption}</div>
+              </figure>
+            )
+          })}
 
-                <ul className="dots">
-                    {images.map((image, index) => (
-                    <li
-                        className={`dot ${currentSlideIndex === index ? 'active' : ''}`}
-                        key={index}
-                        onClick={()=>changeSlide(index)}
-                    >
-                    </li>
-                    ))}
-                </ul>
-            </div>
-        </>
-    )
+          <button className="prev" onClick={() => decrementSlide()}>
+            &#10094;
+          </button>
+          <button className="next" onClick={() => incrementSlide()}>
+            &#10095;
+          </button>
+        </div>
+        <br />
+
+        <ul className="dots">
+          {images.map((image, index) => (
+            <li
+              className={`dot ${currentSlideIndex === index ? "active" : ""}`}
+              key={index}
+              onClick={() => changeSlide(index)}
+            ></li>
+          ))}
+        </ul>
+      </div>
+    </>
+  )
 }
 
 Slideshow.propTypes = {
-    images: PropTypes.arrayOf(PropTypes.shape({
-        src: PropTypes.string,
-        alt: PropTypes.string,
-        caption: PropTypes.string
-    })),
-    imageURLs: PropTypes.object
+  images: PropTypes.arrayOf(
+    PropTypes.shape({
+      src: PropTypes.string,
+      alt: PropTypes.string,
+      caption: PropTypes.string
+    })
+  ),
+  imageURLs: PropTypes.object
 }
 
 export default Slideshow
